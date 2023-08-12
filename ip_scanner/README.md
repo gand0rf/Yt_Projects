81c3193ff17189f72170f5936d154b2e8187e7e0
Gand0rf <98537873+gand0rf@users.noreply.github.com>
2023-08-12 19:27:55 -0400

    Inital upload

---
+++ ip_scanner/README.md
@@ -0,0 +1,17 @@
+Hello!
+
+This is the code file for the first project on my Youtube cahnnel.
+
+If you found this through one of my videos, thanks for watching and stopping by.
+
+If not, thank you for stopping by and having a look.
+
+Links to vidoes:
+
+Part 1: https://youtu.be/R_hZk2P7DIo
+Part 2: https://youtu.be/OszZ7YvUmow
+Part 3: https://youtu.be/b5YjidWT2Qw
+
+TODO:
+
+1. Add comments to the code. I really need to get better about this...
---
+++ ip_scanner/ip_scan.py
@@ -0,0 +1,30 @@
+import subprocess, time
+from multiprocessing.pool import ThreadPool
+
+def ping(ip):
+    result = subprocess.Popen(['ping','-c','1','-s','1','-w','1',f'192.168.1.{ip}'],stdout=subprocess.PIPE)
+    if b'1 received' in result.communicate()[0]:
+        return f'192.168.1.{ip}'
+    
+def ip_clean(ip_list):
+    clean_list = [ip for ip in ip_list if ip != None]
+    return clean_list
+
+def thread_scan():
+    print('\nStaring thread scan...')
+    with ThreadPool() as pool:
+        results = pool.map(ping, range(1,255))
+    print('\nThread scan is complete...')
+    return results
+
+if __name__ == '__main__':
+
+    start_time = time.perf_counter()
+    alive_ip_list = ip_clean(thread_scan())
+    end_time = time.perf_counter()
+
+    print(f'\nFound {len(alive_ip_list)} ips:')
+    for ip in alive_ip_list:
+        print(f'\t{ip} is alive')
+    print(f'\nTime taken: {"{:.2f}".format(end_time-start_time)} seconds')
+
