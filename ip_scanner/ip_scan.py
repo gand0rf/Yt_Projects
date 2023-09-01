14938306c7ec4c1769b3bcbabb05f24d7b75090c
Gand0rf <gand0rf@protonmail.com>
2023-09-01 18:02:10 -0400

    updates

--- ip_scanner/ip_scan.py
+++ ip_scanner/ip_scan.py
@@ -1,11 +1,12 @@
 import subprocess, time
 from multiprocessing.pool import ThreadPool
 
-def ping(ip):
+
+def ping(ip: int):
     result = subprocess.Popen(['ping','-c','1','-s','1','-w','1',f'192.168.1.{ip}'],stdout=subprocess.PIPE)
     if b'1 received' in result.communicate()[0]:
         return f'192.168.1.{ip}'
-    
+
 def ip_clean(ip_list):
     clean_list = [ip for ip in ip_list if ip != None]
     return clean_list
@@ -15,12 +16,12 @@ def thread_scan():
     with ThreadPool() as pool:
         results = pool.map(ping, range(1,255))
     print('\nThread scan is complete...')
-    return results
+    return ip_clean(results)
 
 if __name__ == '__main__':
 
     start_time = time.perf_counter()
-    alive_ip_list = ip_clean(thread_scan())
+    alive_ip_list = thread_scan()
     end_time = time.perf_counter()
 
     print(f'\nFound {len(alive_ip_list)} ips:')
--- nmap_script/nmap_script.py
+++ nmap_script/nmap_script.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import subprocess
 import ipaddress
@@ -10,16 +12,16 @@ def help():
             '\nprovided, port will be set to "-" for all ports'
             '\n\nOptions:'
             '\n\t-h: Prints this help statment.'
-            '\n\t 1: Runs namp with no options.'
-            '\n\t    nmap [ipaddress]'
-            '\n\t 2: Runs nmap with the top 100 ports option.'
-            '\n\t    nmap -F [ipaddress]'
-            '\n\t 3: Runs nmap with assume host is alive option.'
-            '\n\t    nmap -Pn [ipaddress]'
-            '\n\t 4: Runs nmap with assume host is alive option and top 100 ports.'
+            '\n\t 1: Runs nmap with assume host is alive option and top 100 ports.'
             '\n\t    nmap -Pn  -F [ipaddress]'
-            '\n\t 5: Runs nmap with scan on traget port only.'
-            '\n\t    nmap -p [port] [ipaddress]'
+            '\n\t 2: Runs nmap with assume host is alive and specific port/ports to scan'
+            '\n\t    nmap -Pn -p [port] [ipaddress]'
+            '\n\t 3: Runs nmap with OS detection, version detection, script scanning, and traceroute'
+            '\n\t    nmap -Pn -A [ipaddress]'
+            '\n\t 4: Runs nmap with fragmented packages'
+            '\n\t    nmap -Pn -f [ipaddress]'
+            '\n\t 5: Runs nmap with a smb enumeration script'
+            '\n\t    nmap -Pn --script=smb-enum-shares.nse -p [port] [ipaddress]'
             '\n')
     
 def ip_check(ip):
@@ -28,51 +30,62 @@ def ip_check(ip):
         return 1
     except ValueError:
         return 0
-
-try:
-    if sys.argv[1] != '-h':
-
-        selection = sys.argv[1]
-
-        if ip_check(sys.argv[2]) == 1:
-            ip = sys.argv[2]
+    
+def port_check(port):
+    if len(port) == 1:
+        port = port[0].split(',')
+        print(port)
+    for port_num in port:
+        if int(port_num) > 0 and int(port_num) <= 65535:
+            continue
         else:
-            print('\nInvalid ip address\n')
+            print('\nInvalid port given\n')
+            sys.exit()
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) < 3:
+            print('\nPlease verify your input\n')
             help()
             sys.exit()
 
-        if sys.argv[3] != '' and int(sys.argv[3]) <= 65535 and int(sys.argv[3]) > 0:
-            port = sys.argv[3]
-        elif int(sys.argv[3]) > 65535 or int(sys.argv[3]) < 0:
-            print('\nInvalid port number\n')
-            help()
-            sys.exit()
-        else:
-            port = '-'
+        if sys.argv[1] != '-h':
 
-        match selection:
-            case '1':
-                command = subprocess.Popen(['nmap',f'{selection}',f'{ip}'])
-                command.wait()
-            case '2':
-                command = subprocess.Popen(['nmap','-F',f'{selection}',f'{ip}'])
-                command.wait()
-            case '3':
-                command = subprocess.Popen(['nmap','-Pn',f'{selection}',f'{ip}'])
-                command.wait()
-            case '4':
-                command = subprocess.Popen(['nmap','-Pn','-F',f'{selection}',f'{ip}'])
-                command.wait()
-            case '5':
-                command = subprocess.Popen(['nmap','-p', f'{port}',f'{selection}',f'{ip}'])
-                command.wait()
-            case _:
-                print('\nNot a valid nmap option\n')
+            selection = sys.argv[1]
+
+            if ip_check(sys.argv[2]) == 1:
+                ip = sys.argv[2]
+            else:
+                print('\nInvalid ip address\n')
                 help()
-    elif sys.argv[1] == '-h':
-        help()
-except IndexError:
-    print('\nPlease enter valid option and ip address for program\n')
-    help()
-except Exception as error:
-    print(f'\nError has occurred:\n\n{error}')
+                sys.exit()
+
+            if len(sys.argv) > 3 and port_check(sys.argv[3:]) == True:
+                port = ''.join(sys.argv[3:])
+
+
+            match selection:
+                case '1':
+                    command = subprocess.Popen(['nmap','-Pn','-F',f'{ip}'])
+                    command.wait()
+                case '2':
+                    command = subprocess.Popen(['nmap','-Pn','-p', f'{port}',f'{ip}'])
+                    command.wait()
+                case '3':
+                    command = subprocess.Popen(['nmap','-Pn','-A',f'{ip}'])
+                    command.wait()
+                case '4':
+                    command = subprocess.Popen(['nmap','-Pn','-f',f'{ip}'])
+                    command.wait()
+                case '5':
+                    command = subprocess.Popen(['nmap','-Pn','--script=smb-enum-shares.nse','-p', f'{port}',f'{ip}'])
+                    command.wait()
+                case _:
+                    print('\nNot a valid nmap option\n')
+                    help()
+        elif sys.argv[1] == '-h':
+            help()
+    except Exception as error:
+        print(f'\nError has occurred:\n\n{error}')
